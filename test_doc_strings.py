92c39b1a35acff0cde5cb251718b2d0919ea0b14
Wrench, David <David.Wrench@McKesson.com>
2020-03-25 09:26:26 -0700

    Add: init files

---
+++ test_doc_strings.py
@@ -0,0 +1,29 @@
+"""Testing Documentation Strings
+
+"""
+
+
+def my_function(a, b):
+	"""
+
+	Args:
+		a:
+		b:
+
+	Returns:
+
+	"""
+	c = a + b
+	return c
+
+
+def new_func(name: str) -> str:
+	"""
+	
+	Args:
+		name ():
+
+	Returns:
+
+	"""
+	return name
